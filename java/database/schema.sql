453f9e5b578e14b18b2115c23fbda35092eaaa08
feysal mohamed <feysal7002@gmail.com>
2023-08-05 11:12:21 -0400

    push test

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, books_table;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,29 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE books_table (
+
+book_name varchar(200) NOT NULL,
+	author varchar (100) NOT NULL,
+	author_second varchar(100) NULL,
+	description varchar(260) NOT NULL,
+	series varchar(50) NULL,
+	book_id serial NOT NULL,
+	release_date DATE NOT NULL,
+	date_added_to_collection DATE NULL
+	
+	
+);
+
+CREATE TABLE genre_table (
+
+	book_id serial NOT NULL,
+	genre_id serial NOT NULL,
+	genre_name varchar(50) NOT NULL
+
+
 );
 
 COMMIT TRANSACTION;
