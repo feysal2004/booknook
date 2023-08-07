7452c306e738f252572d440aa1112c00f9c448c0
feysal mohamed <feysal7002@gmail.com>
2023-08-07 09:49:59 -0400

    push fixedd

--- vue/src/components/Forum.vue
+++ vue/src/components/Forum.vue
@@ -2,23 +2,10 @@
   <div class="add-new-message">
       
       <button class ="btnAddMessage"> Add Message</button>
-      
-      <router-link :to="{name:'postMessage'}">Create New Post</router-link>
-
-    <div>
-        
-        <h3 class="message-title">{{message.title}}</h3>
-        <p class="message-body">{{message.messageText}}</p>
-
-        <router-link :to="{name: 'EditMessage'}" class="btnEditMessage"> Edit Message</router-link>
-        <button class="btnDeleteMessage" v-on:click ="deleteMessage(message.id)">Delete</button>
+    
 
 
-        
-
-
-    </div> 
-
+    
 
 
   </div>
