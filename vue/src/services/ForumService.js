ebbc6679bbcde8f6c084f78f8f38024cc2c1f9e8
feysal mohamed <feysal7002@gmail.com>
2023-08-09 12:38:18 -0400

    helpedup2date

--- vue/src/services/ForumService.js
+++ vue/src/services/ForumService.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
-const http = axios.create({
-    baseURL: "http://localhost:9000"
-  });
+// const http = axios.create({
+//     baseURL: "http://localhost:9000"
+//   });
 
   export default{
 
     messageList(topicId){
-        return http.get(`/topics/message/${topicId}`)
+        return axios.get(`/topics/message/${topicId}`)
     },
 
     getForumMessageId(messageId){
-        return http.get(`/topics/${messageId}`);
+        return axios.get(`/topics/${messageId}`);
     },
 
     createForumMessage(topicId, message_text){
-        return http.post(`/topics/message/${topicId}`, message_text)
+        return axios.post(`/topics/message/${topicId}`, message_text)
     },
 
     getTopicName(topicId){
-        return http.get(`/topics/message/${topicId}/name`);
+        return axios.get(`/topics/message/${topicId}/name`);
     }
 
 
