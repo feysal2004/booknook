19b6ceb41d0c5dfc134c81c961d5a1bfa3fad43b
Matthew Kowalski <matthewjkowalski88@gmail.com>
2023-08-09 23:28:10 -0400

    sprint 2 push start of book api

---
+++ java/src/main/java/com/techelevator/controller/GoogleBookApiController.java
@@ -0,0 +1,9 @@
+package com.techelevator.controller;
+
+public class GoogleBookApiController {
+
+
+    String url = "https://www.googleapis.com/books/";
+    String apiKey = "AIzaSyD7CXjdwbiWpjFADwOGsX0XZoJPXUgej7c";
+
+}
