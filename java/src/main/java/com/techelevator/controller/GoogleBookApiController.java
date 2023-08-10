a3e53456f12021492c438d01b07aed14529ddfe0
Matthew Kowalski <matthewjkowalski88@gmail.com>
2023-08-10 09:05:04 -0400

    sprint 2 push start of book api

--- java/src/main/java/com/techelevator/controller/GoogleBookApiController.java
+++ java/src/main/java/com/techelevator/controller/GoogleBookApiController.java
@@ -1,9 +1,14 @@
 package com.techelevator.controller;
 
+import java.time.LocalDate;
+
 public class GoogleBookApiController {
 
 
     String url = "https://www.googleapis.com/books/";
+    String url2 = "https://www.googleapis.com/books/v1/volumes?q=";
     String apiKey = "AIzaSyD7CXjdwbiWpjFADwOGsX0XZoJPXUgej7c";
+
+
 
 }
---
+++ java/src/main/java/com/techelevator/model/GoogleBooksApi.java
@@ -0,0 +1,21 @@
+package com.techelevator.model;
+
+import java.time.LocalDate;
+
+public class GoogleBooksApi {
+
+    String url = "https://www.googleapis.com/books/";
+    String url2 = "https://www.googleapis.com/books/v1/volumes?q=";
+    String apiKey = "AIzaSyD7CXjdwbiWpjFADwOGsX0XZoJPXUgej7c";
+
+    String book_name;
+    String author;
+    String description;
+    String series;
+    int book_id;
+    LocalDate release_date;
+    LocalDate date_added_to_collection;
+
+
+
+}
