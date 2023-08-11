78f8e5358428c974113a35bde2913a48a329a8f3
feysal mohamed <feysal7002@gmail.com>
2023-08-11 16:56:20 -0400

    5pm  second push

--- vue/src/components/ForumMessages.vue
+++ vue/src/components/ForumMessages.vue
@@ -50,7 +50,7 @@
               <td class="edit-button-cell"  >
                   <button class="edit-button" v-on:click.prevent.stop="editMessage(message.message_id, index)" >Edit</button>
                   <button >Save Message</button>
-                  <button class="delete-message-button" v-on:click="deleteMessage(message.message_id)" v-if="$store.state.user.username === 'admin'">Delete Topic</button>
+                  <button class="delete-message-button" v-on:click="deleteMessage(message.message_id)" v-if="$store.state.user.username === 'admin'">Delete Message</button>
                   
               </td>
             </tr> 
