5accaea20c2049ee940eac4bc9499c5de6c0af9a
Matthew Kowalski <matthewjkowalski88@gmail.com>
2023-08-14 12:09:27 -0400

    trivia somewhat working

---
+++ vue/src/components/HomeTrivia.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+// import triviaService from '../services/TriviaService';
+
+export default {
+    data() {
+        return {
+            difficultyChoice: "",
+            showAnswer: false
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
